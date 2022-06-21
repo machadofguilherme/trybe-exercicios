49869e8f0a318911d2135eb0b245affdb933bcd0
Guilherme Machado <machadofguilherme@gmail.com>
2022-06-21 15:31:39 -0300

    Bloco 4 - Aula 1 - Exercício 1

---
+++ Fundamentos/JavaScript/Bloco 4/Aula 1/script.js
@@ -0,0 +1,13 @@
+const myName = 'Guilherme';
+const birthCity = 'Porto Alegre';
+let birthYear = 1992;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+// birthCity = 'X';
+
+console.log('Constantes são constantes, portanto, são inalteráveis.');
