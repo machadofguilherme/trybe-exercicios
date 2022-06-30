34915afd10dd0c734a70c54ccb7547af0c92bf49
Guilherme Machado <machadofguilherme@gmail.com>
2022-06-30 18:43:41 -0300

    Bloco 5 - Aula 3 - Parte 2.

---
+++ Fundamentos/JavaScript/Bloco 5/Aula 3/script.js
@@ -0,0 +1,43 @@
+function createDaysOfTheWeek() {
+  const weekDays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+  ];
+  const weekDaysList = document.querySelector(".week-days");
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement("li");
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  }
+}
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+// 🚀 Exercício 1:
+// Crie um calendário dinamicamente.
+// O array decemberDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Sua função deve criar dinamicamente cada dia do calendário e os adicionar dentro da tag <ul>.
+
+let numbersWeek = document.querySelector('#days');
+let decemberDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 
+  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 
+  23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+
+window.onload = () => {
+    for (let index = 0; index < decemberDaysList.length; index += 1) {
+        let newDays = document.createElement('li');
+        newDays.innerText = decemberDaysList[index];
+        numbersWeek.appendChild(newDays);
+    }
+}
