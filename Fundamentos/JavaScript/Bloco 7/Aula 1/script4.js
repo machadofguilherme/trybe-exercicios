a4a6e195fd9a33a651d2b3e3e73e041b8f01f979
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-14 22:04:05 -0300

    Bloco 7 - Aula 1 - Exercício 5.

---
+++ Fundamentos/JavaScript/Bloco 7/Aula 1/script4.js
@@ -0,0 +1,10 @@
+let newPhrase;
+
+const replaceX = (name) => {
+  const phrase = "Tryber x aqui!";
+
+  newPhrase = phrase.replace("x", name);
+  console.log(newPhrase);
+};
+
+replaceX("Guilherme");
