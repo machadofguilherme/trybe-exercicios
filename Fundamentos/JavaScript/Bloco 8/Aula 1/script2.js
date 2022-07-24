f200301d9c27769bd6fcbeb669fb04c38e69cfdc
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-23 21:07:02 -0300

    Bloco 8 - Aula 1 - Exercício 2 e 3.

--- Fundamentos/JavaScript/Bloco 8/Aula 1/script2.js
+++ Fundamentos/JavaScript/Bloco 8/Aula 1/script2.js
@@ -1,3 +1,5 @@
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id. A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com.
+
 const newEmployees = () => {
   const employees = {
     id1: returnPerson('Pedro Guerra'),
@@ -8,7 +10,9 @@ const newEmployees = () => {
 };
 
 const returnPerson = (name) => {
-  console.log( { nomeCompleto: name, email: `${name}@trybe.com`} );
+  let lowerName = name.toLowerCase();
+  lowerName = lowerName.replace(' ', '-');
+  console.log( { nomeCompleto: name, email: `${lowerName}@trybe.com`} );
 }
 
 newEmployees();
