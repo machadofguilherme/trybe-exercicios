8ee68730b1281f97323c9164fbe40adf7a39ff93
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-25 18:36:12 -0300

    Bloco 8 - Aula 3 - Exercício 6.

---
+++ Fundamentos/JavaScript/Bloco 8/Aula 3/script6.js
@@ -0,0 +1,79 @@
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+// 🚀 5 - Crie um array em ordem alfabética apenas com os nomes de todas as pessoas autoras de ficção científica ou fantasia.
+
+function fantasyOrScienceFictionAuthors() {
+  // escreva seu código aqui
+  const ficAuthors = books.filter((e) => {
+    return e.genre === 'Ficção Científica' ||
+    e.genre === 'Fantasia'
+  });
+  
+  return ficAuthors.sort((a, b) => 
+  a.author.name > b.author.name ? 1 : -1)
+  .map((e) => e.author.name);
+}
+
+console.log(fantasyOrScienceFictionAuthors());
